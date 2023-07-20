c94534f75c1e868aa0bbcc103ec5cca2b9b9d0b5
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-20 04:13:44 -0700

    task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int track = 1, temp = 0;
-	size_t i = 0, j = 1;
+	int temp = 0;
+	size_t i = 0, count = 0;
+	count = size;
 
-	if (size < 2)
+	if (count < 2)
 		exit(EXIT_SUCCESS);
-	while (track != 0)
+	while (count > 1)
 	{
-		track = 0;
-		for(i = 0; i < size; i++)
+		for(i = 0; i < count - 1; i++)
 		{
-			for(j = 1; j <= size; j++)
+			if (array[i] > array[i + 1])
 			{
-				if (array[i] > array[j])
-				{
-					temp = array[j];
-					array[j] = array[i];
-					array[i] = temp;
-					print_array(array, size);
-					track = 1;
-				}
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
 			}
+			count--;
 		}
 	}
 }
