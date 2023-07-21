ccd167e1d89d1d5c68997e0bd552ead0198ad336
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-21 07:18:08 -0700

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array of integers
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: nothing is returned
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min_index = 0;
+	int min = 0, tmp = 0;
+
+	for (i = 0; i < size-1; i++)
+	{
+		min = array[i];
+		min_index = i;
+		for (j = i; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				min_index = j;
+			}
+		}
+		tmp = array[i];
+		array[i] = array[min_index];
+		array[min_index] = tmp;
+		print_array(array, size);
+	}
+}
