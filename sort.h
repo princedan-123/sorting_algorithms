70b01b4dc89a476b96f6f7af2191a3d5af43c890
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-21 03:21:18 -0700

    corrected header file

--- sort.h
+++ sort.h
@@ -9,9 +9,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 #include <stddef.h>
@@ -21,4 +21,3 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 #endif
-
