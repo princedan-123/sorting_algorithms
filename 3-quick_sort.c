a50c20d2b07f85002acef1b62a0f3fdcb8051393
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-07-23 16:17:33 +0000

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
  * quick_sort - sorts an array in ascending order
@@ -9,11 +10,28 @@
 
 void quick_sort(int *array, size_t size)
 {
-	int tmp = 0, pivot_index = 0, pivot_value           
+	int tmp = 0, pivot_value = array[size - 1], p_index = 0;
 	size_t j = 0, i = -1;
 
-	for (j = 0; j < size; j++)
+	if (size <= 1)
+		exit(EXIT_SUCCESS);
+
+	for (j = 0; j < size - 1; j++)
 	{
-		if (array[j] < 
+		if (array[j] <= pivot_value)
+		{
+			i++;
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] =  tmp;
+		}
 	}
+	i++;
+	p_index = i;
+	tmp = array[i];
+	array[i] = pivot_value;
+	pivot_value = tmp;
+	print_array(array, size);
+	quick_sort(array, p_index - 1);
+	quick_sort(array + p_index, size - p_index - 1);
 }
