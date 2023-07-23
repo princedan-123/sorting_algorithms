1f174b578a54b2f8750a657d26ff8198abfb1fda
daniel ebuka mabia <danielebukamabia@gmail.com>
2023-07-23 07:21:11 -0700

    file upload

---
+++ 3-quick_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array in ascending order
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: void
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int tmp = 0, pivot_index = 0, pivot_value           
+	size_t j = 0, i = -1;
+
+	for (j = 0; j < size; j++)
+	{
+		if (array[j] < 
+	}
+}
