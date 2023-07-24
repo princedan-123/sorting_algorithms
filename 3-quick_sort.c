8b537b9488b95a0178399a0ec4eeba70251a54da
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-07-24 13:56:44 +0000

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@
 
 void quick_sort(int *array, size_t size)
 {
-	int tmp = 0, pivot_value = array[size - 1], p_index = 0;
+	int tmp = 0, pivot_value = array[size - 1];
 	size_t j = 0, i = -1;
 
 	if (size <= 1)
@@ -27,12 +27,10 @@ void quick_sort(int *array, size_t size)
 		}
 	}
 	i++;
-	p_index = i;
 	tmp = array[i];
 	array[i] = array[size -1];
 	array[size - 1] = tmp;
 	print_array(array, size);
-	if (p_index > 0)
-		quick_sort(array, p_index -1);
-	quick_sort(array + p_index + 1, size - p_index - 1);
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
 }
